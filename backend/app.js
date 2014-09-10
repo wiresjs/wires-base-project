81aab560526f95c3c7332ade4902db90214428fc
Ivan Orlov <nchanged@gmail.com>
2014-09-10 13:17:27 +0300

    Initial version

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var domain = require('wires-domain');
+var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser('your secret here'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+app.use('/', express.static(path.join(__dirname, '../frontend')));
+domain.setAdapter(domain.adapters.File);
+
+
+
+var Item = domain.models.BaseModel.extend({
+	name : 'items',
+	schema : {
+		id : {},
+		name : {
+			required : function(name) {
+				if (name === 'pukka') {
+					throw new domain.errors.Validate('Stop doing this already!');
+				}
+			}
+		},
+		password : {
+			type : 'string',
+			hidden : true
+		}
+	}
+});
+
+domain.rest.Path('/api/items/:id?', Item);
+
+
+app.use(domain.rest.Service);
+app.listen(8888);
+console.log('listening on port:8888');
---
+++ frontend/bower_components/wires.js/README.md
@@ -0,0 +1,18 @@
+wires.js
+========
+
+Wires.js - Wires your DOM
+
+
+# How to install
+
+Install it from bower:
+
+    bower install wires.js
+    
+It will install dependencies, that you need to include into your project along with build.min.js from build folder
+
+* Jquery
+* async
+* lodash
+
